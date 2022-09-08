10e34b510790b7e426e283d42edf6b9ee41eaa3d
tythomas21 <66636644+tythomas21@users.noreply.github.com>
2022-09-08 14:13:34 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("#greetingOutput").text("");
-
-  $("#sendName").click(function () {
-    //console.log($("#fname").val());
-    $("#greetingOutput").text("Hello " + $("#fname").val());
-  });
-});
